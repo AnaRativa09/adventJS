79cce75b1e33e146258793f3cc49b763484a61ce
AnaRativa09 <anarativa09@gmail.com>
2021-12-02 20:56:54 -0500

    Create package.json, install eslint dependency and fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    'no-plusplus': 'off'
+  },
+};
---
+++ exercises/01-countingSheeps/index.js
@@ -0,0 +1,24 @@
+function countingSheeps(sheeps) {
+  const filterSheeps = sheeps.filter((sheep) => {
+    const sheepName = sheep.name.toLowerCase();
+
+    return (
+      sheep.color === 'rojo'
+      && sheepName.includes('n')
+      && sheepName.includes('a')
+    );
+  });
+
+  return filterSheeps;
+}
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo' },
+  { name: 'Ki N M', color: 'rojo' },
+  { name: 'navidad', color: 'rojo' },
+];
+
+console.log(countingSheeps(ovejas));
