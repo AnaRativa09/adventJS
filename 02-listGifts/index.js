a37d3dc8ddd390f9364c7ecbe96b6b9d4a7bb5c2
AnaRativa09 <anarativa09@gmail.com>
2021-12-02 20:29:27 -0500

    feat: add day-02 exercise

---
+++ 02-listGifts/index.js
@@ -0,0 +1,16 @@
+function listGifts(letter) {
+  const arrLetter = letter.split(' ');
+  const objectGifts = {};
+
+  for (let i = 0; i < arrLetter.length; i++) {
+    if (arrLetter[i] != '' && !arrLetter[i].startsWith('_')) {
+      objectGifts[arrLetter[i]] ? objectGifts[arrLetter[i]]++ : objectGifts[arrLetter[i]] = 1
+    }
+  }
+
+  return objectGifts;
+}
+
+const carta = 'bici coche balón _playstation bici coche peluche  holi'
+
+console.log(listGifts(carta));
