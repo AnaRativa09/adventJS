b23d3153b3133191d078335ccc6f99f00cd71115
AnaRativa09 <anarativa09@gmail.com>
2021-12-02 21:29:36 -0500

    feat: new solution exercise day02

--- exercises/02-listGifts/index.js
+++ exercises/02-listGifts/index.js
@@ -1,12 +1,30 @@
 function listGifts(letter) {
   const arrLetter = letter.split(' ');
+
   const objectGifts = {};
 
   for (let i = 0; i < arrLetter.length; i++) {
     if (arrLetter[i] !== '' && !arrLetter[i].startsWith('_')) {
-      objectGifts[arrLetter[i]] ? objectGifts[arrLetter[i]]++ : objectGifts[arrLetter[i]] = 1;
+      objectGifts[arrLetter[i]]
+        ? objectGifts[arrLetter[i]] += 1
+        : objectGifts[arrLetter[i]] = 1;
     }
   }
+
+  return objectGifts;
+}
+
+function listGifts2(letter) {
+  const arrGifts = letter.split(' ');
+  const arrFilterGifts = arrGifts.filter((gift) => gift !== '' && !gift.startsWith('_'));
+
+  const objectGifts = {};
+
+  arrFilterGifts.forEach((gift) => {
+    objectGifts[gift]
+      ? objectGifts[gift] += 1
+      : objectGifts[gift] = 1;
+  })
 
   return objectGifts;
 }
@@ -14,3 +32,4 @@ function listGifts(letter) {
 const carta = 'bici coche balón _playstation bici coche peluche  holi';
 
 console.log(listGifts(carta));
+console.log(listGifts2(carta));
